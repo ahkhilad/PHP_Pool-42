33aab61dfcf54f679568880afac57a7dd86617ae
Ahmed Khilad <ahkhilad@e1r8p10.1337.ma>
2019-12-11 14:50:25 +0100

    ex02 re-make

--- day01/ex02/oddeven.php
+++ day01/ex02/oddeven.php
@@ -13,31 +13,14 @@ while (1)
     if (is_numeric($var)) 
     {
         $var = trim($var);
-        if (strlen($var) >= 19)
-        {
-            $i = 19;
-            while ($i < strlen($var))
-            {
-                if (intval($var[$i]) % 2 === 0)
-                    $r = 1;
-                else
-                    $r = 0;
-                $i++;
-            }
-        }
-        if (intval($var) % 2 === 0 || $r == 1) 
-        {
+        $num = strlen($var);
+        if (intval($var[$num - 1]) % 2 === 0)
             echo "The number $var is even\n";
-        }
         else 
-        {
             echo "The number $var is odd\n";
-        }
     }
     else 
-    {
         echo "'$sp' is not a number\n";
-    }
 }
 
 ?>
